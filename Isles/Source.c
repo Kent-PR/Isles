39448a2008de76537750130d58d754e6d5d2f784
KentPR <Promaster081@gmail.com>
2019-12-12 10:58:42 +0300

    Created wokring input.

--- Isles/Source.c
+++ Isles/Source.c
@@ -2,10 +2,48 @@
 
 #include <stdio.h>
 
+char matrix[8][8];
+int data_h[8][4];
+int data_v[8][4];
+int N;
+
+void input(void);
+
 int main(void)
 {
-
+	input();
 
 
 	return 0;
+}
+
+void input(void)
+{
+	printf("¬ведите N: ");
+	scanf("%d", &N);
+	for (int hor = 0; hor < N; hor++)
+	{
+		int tmp;
+		int i = 0;
+		while (1)
+		{
+			scanf("%d", &tmp);
+			data_h[hor][i] = tmp;			
+			i++;
+			if (!tmp) break;
+		}
+	}
+
+	for (int vert = 0; vert < N; vert++)
+	{
+		int tmp;
+		int i = 0;
+		while (1)
+		{
+			scanf("%d", &tmp);
+			data_v[vert][i] = tmp;			
+			i++;
+			if (!tmp) break;
+		}
+	}
 }
