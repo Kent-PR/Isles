25f0dfd0ca1bac86707f992162a58d7e2dfa87ca
KentPR <Promaster081@gmail.com>
2019-12-12 19:04:48 +0300

    Corrected input

--- Isles/Source.c
+++ Isles/Source.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 char matrix[8][8];
-int data_h[8][4];
-int data_v[8][4];
+int data_h[8][5];
+int data_v[8][5];
 int N;
 
 //in out
@@ -12,18 +12,82 @@ void input(void);
 void output(void);
 //
 
+//file
+FILE* opening(void);
+//
+
+//tmp
+void create_data(FILE* fin);
+void rus(void);
+//
 
 int main(void)
 {
+	rus();
 	input();
 
 
 	return 0;
 }
 
+void rus(void)
+{
+	system("chcp 1251");
+	system("cls");
+}
+
+void create_data(FILE* fin)
+{
+	N = fgetc(fin) - 48;
+	fgetc(fin);
+	char tmp[4]; int counter = 0;
+	char c; int i;
+	while ((c = fgetc(fin)) != EOF)
+	{
+		i = 0;
+		while ((c != '\n') && (c != EOF))
+		{
+			if (c != ' ') 
+			{
+				if (counter < N)
+				{
+					data_h[counter][i] = c-48;
+					i++;
+				}
+				else
+				{
+					data_v[counter-N][i] = c-48;
+					i++;
+				}
+			}
+			c = fgetc(fin);
+		}
+		counter++;
+	}
+}
+
+FILE* opening(void)
+{
+	char name[100];//отводим массив под имя файла.
+	printf("Ввведите имя файла: ");
+	gets(name);
+	FILE* file_in = fopen(name, "r");
+
+	while (file_in == NULL)// если файл не открылся.
+	{
+		printf("Допущена ошибка в имени файла!\n");
+		printf("Введите имя файла: ");
+		gets(name);
+		file_in = fopen(name, "r");
+	}
+	return file_in;//возвращаем указатель на поток.
+}
+
 void input(void)
 {
-	printf("Введите N: ");
+	create_data(opening());
+
+	/*printf("Введите N: ");
 	scanf("%d", &N);
 	for (int hor = 0; hor < N; hor++)
 	{
@@ -49,7 +113,7 @@ void input(void)
 			i++;
 			if (!tmp) break;
 		}
-	}
+	}*/
 }
 
 void output(void)
