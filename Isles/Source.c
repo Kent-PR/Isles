f7e593b72a6a0d955202cc9178da65f70834457c
KentPR <Promaster081@gmail.com>
2019-12-21 01:34:48 +0300

    Deleted comments

--- Isles/Source.c
+++ Isles/Source.c
@@ -338,34 +338,6 @@ FILE* opening(void)
 void input(void)
 {
 	create_data(opening());
-
-	/*printf("¬ведите N: ");
-	scanf("%d", &N);
-	for (int hor = 0; hor < N; hor++)
-	{
-		int tmp;
-		int i = 0;
-		while (1)
-		{
-			scanf("%d", &tmp);
-			data_h[hor][i] = tmp;			
-			i++;
-			if (!tmp) break;
-		}
-	}
-
-	for (int vert = 0; vert < N; vert++)
-	{
-		int tmp;
-		int i = 0;
-		while (1)
-		{
-			scanf("%d", &tmp);
-			data_v[vert][i] = tmp;			
-			i++;
-			if (!tmp) break;
-		}
-	}*/
 }
 
 void print(void)
